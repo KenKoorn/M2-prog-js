06d058cd44adfc931ce147d4db21c320f35f1bca
KenKoorn <115067945+KenKoorn@users.noreply.github.com>
2022-12-22 09:53:29 +0100

    Add files via upload

---
+++ Java  Dino/app.js
@@ -0,0 +1,5 @@
+class Dino {
+  constructor(naam) {
+    this.naam = naam;
+  }
+}
---
+++ Java 1/app.js
@@ -0,0 +1,22 @@
+class App {
+  runApplication() {
+    console.log("hello world");
+    let appnaam = "Java";
+    let versienummer = 0.9;
+    let versiedatum = "12-09-2022";
+    let autheur = "Ken";
+    let copyright = "Je kan niet dingen kopieren van andere mensen.";
+    let distributeur = "Amazon";
+    let darkmode = true;
+    console.log("hello world");
+    console.log(appnaam);
+    console.log(versienummer);
+    console.log(versiedatum);
+    console.log(autheur);
+    console.log(copyright);
+    console.log(distributeur);
+  }
+}
+
+let app = new App();
+app.runApplication();
